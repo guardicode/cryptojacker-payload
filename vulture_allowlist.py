fb9ce3be2e64e4cdfc17ab2776af18d2244ac940
Ilija Lazoroski <ilija.la@live.com>
2024-07-09 10:00:00 +0000

    Initialize the repository

---
+++ vulture_allowlist.py
@@ -0,0 +1,10 @@
+from cryptojacker_plyload.cpu_utilizer import CPUUtilizer
+
+from cryptojacker_payload.plugin import Plugin
+
+Plugin
+Plugin.plugin_name
+Plugin.kwargs
+
+CPUUtilizer.restype
+CPUUtilizer.argtypes
